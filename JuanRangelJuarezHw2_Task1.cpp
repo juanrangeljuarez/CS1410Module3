b19f559ad21e018ed74f005fca7c8693a7e5b4b5
juan <juanrangel1@mail.weber.edu>
2017-09-19 13:00:55 -0600

    First Commit

---
+++ JuanRangelJuarezHw2_Task1.cpp
@@ -0,0 +1,19 @@
+// File: JuanRangelJuarezHw2_Task1
+// Created by Juan Rangel Juarez on 9/19/17 for CS1410.
+// Copyright (c) 2017 WSU
+//
+#include <iostream>
+
+using namespace std;
+
+// Constants
+
+// Prototypes
+
+// Main Program Program
+int main(void)
+{
+
+    return 0;
+}
+// Function Definitions
